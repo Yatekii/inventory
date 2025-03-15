1bc1fb6209210bfb82db3156116d38c38f6e9348
Noah Hüsser <noah@huesser.dev>
2025-03-15 23:21:52 +0100

    Initial commit

---
+++ machines/aiur/configuration.nix
@@ -0,0 +1,32 @@
+{
+  imports = [
+    # contains your disk format and partitioning configuration.
+    ../../modules/disko.nix
+    # this file is shared among all machines
+    ../../modules/shared.nix
+  ];
+
+  # This is your user login name.
+  users.users.user.name = "yatekii";
+
+  # Set this for clan commands use ssh i.e. `clan machines update`
+  # If you change the hostname, you need to update this line to root@<new-hostname>
+  # This only works however if you have avahi running on your admin machine else use IP
+  clan.core.networking.targetHost = "root@142.132.172.209";
+
+  # You can get your disk id by running the following command on the installer:
+  # Replace <IP> with the IP of the installer printed on the screen or by running the `ip addr` command.
+  # ssh root@<IP> lsblk --output NAME,ID-LINK,FSTYPE,SIZE,MOUNTPOINT
+  disko.devices.disk.main.device =
+    "/dev/disk/by-id/0QEMU_QEMU_HARDDISK_59606587";
+
+  # IMPORTANT! Add your SSH key here
+  # e.g. > cat ~/.ssh/id_ed25519.pub
+  users.users.root.openssh.authorizedKeys.keys = [''
+    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH/zWoCMabsPjao7AZKfA1jvokjbOBxyGHHKOwTA9krw auraya
+  ''];
+
+  # Zerotier needs one controller to accept new nodes. Once accepted
+  # the controller can be offline and routing still works.
+  clan.core.networking.zerotier.controller.enable = true;
+}
