fe8bcca76e3130e1a9e9664c379acdfe4c17dafc
Noah Hüsser <noah@huesser.dev>
2025-12-01 17:22:39 +0100

    configure git correctly

--- flake/modules/home/yatekii/git.nix
+++ flake/modules/home/yatekii/git.nix
@@ -2,23 +2,25 @@
 {
   programs.git = {
     enable = true;
-    userName = "Noah Hüsser";
-    userEmail = "noah@huesser.dev";
-    aliases = {
-      co = "checkout";
-      caa = "commit --amend -a --no-edit";
-      ca = "commit --amend --no-edit";
-      pf = "push -f";
-      rb = "!git checkout master && git pull && git checkout - && git rebase master";
+    settings = {
+      user = {
+        name = "Noah Hüsser";
+        email = "noah@huesser.dev";
+      };
+      alias = {
+        co = "checkout";
+        caa = "commit --amend -a --no-edit";
+        ca = "commit --amend --no-edit";
+        pf = "push -f";
+        rb = "!git checkout master && git pull && git checkout - && git rebase master";
+      };
+      push.autoSetupRemote = true;
     };
     signing = {
       format = "ssh";
       # TODO: Store this keypair in sops or maybe 1Pass or similar?
       key = "/Users/yatekii/.ssh/id_ed25519.pub";
       signByDefault = true;
-    };
-    extraConfig = {
-      push.autoSetupRemote = true;
     };
   };
 }
