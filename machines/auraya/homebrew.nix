c37d4646f9c354f69b0deb6bab62e0ea8c06bd46
Noah Hüsser <noah@huesser.dev>
2025-12-01 17:30:07 +0100

    only install rosetta2 if enabled

--- machines/auraya/homebrew.nix
+++ machines/auraya/homebrew.nix
@@ -7,11 +7,6 @@
     # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
     enableRosetta = true;
 
-    # We need rosetta installed
-    system.activationScripts.extraActivation.text = ''
-      softwareupdate --install-rosetta --agree-to-license
-    '';
-
     # User owning the Homebrew prefix
     user = "yatekii";
 
@@ -29,4 +24,13 @@
 
   # Align homebrew taps config with nix-homebrew
   homebrew.taps = builtins.attrNames config.nix-homebrew.taps;
+
+  # We need Rosetta installed
+  system.activationScripts.extraActivation.text =
+    if config.nix-homebrew.enableRosetta then
+      ''
+        softwareupdate --install-rosetta --agree-to-license
+      ''
+    else
+      '''';
 }
