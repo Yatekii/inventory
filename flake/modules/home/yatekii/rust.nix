2b2a7ffcb5a3ee7991498431640200d3b6af7bdc
Noah Hüsser <noah@huesser.dev>
2025-12-01 17:23:32 +0100

    install stable rust including common toolchains

--- flake/modules/home/yatekii/rust.nix
+++ flake/modules/home/yatekii/rust.nix
@@ -9,6 +9,18 @@
   ];
 
   home.packages = [
-    pkgs.rust-bin.stable.latest.default
+    (pkgs.rust-bin.stable.latest.default.override {
+      extensions = [
+        "rust-src"
+        "rust-analyzer"
+        "clippy"
+      ];
+      targets = [
+        "aarch64-apple-darwin"
+        "thumbv7em-none-eabihf"
+        "wasm32-unknown-unknown"
+        "riscv32imac-unknown-none-elf"
+      ];
+    })
   ];
 }
