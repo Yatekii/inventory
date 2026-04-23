d0e0659f15b123527fe24f7f577fe900224f09ca
Noah Hüsser <noah@huesser.dev>
2026-04-24 00:46:27 +0200

    darwin yatekii: treefmt reformat

--- modules/darwin/users/yatekii.module.nix
+++ modules/darwin/users/yatekii.module.nix
@@ -14,11 +14,10 @@ in
       # Pull in the portable yatekii config, then layer on the desktop-only
       # modules (launchd-managed apps, GUI tools). NixOS hosts import
       # yatekii.nix without this extra gather so they stay server-shaped.
-      imports =
-        [
-          ../../home/user/yatekii.nix
-        ]
-        ++ gatherModules lib [ ../../home/user/yatekii/desktop ];
+      imports = [
+        ../../home/user/yatekii.nix
+      ]
+      ++ gatherModules lib [ ../../home/user/yatekii/desktop ];
       home.username = "yatekii";
       home.homeDirectory = "/Users/yatekii";
       home.stateVersion = "25.05";
